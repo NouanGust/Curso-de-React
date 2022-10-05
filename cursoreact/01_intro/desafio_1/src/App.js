dec898c6d6213e1a97d4773cf3d2c0ca5eca3dc5
NouanGust <nohan.gnb@gmail.com>
2022-10-05 10:47:53 -0300

    arquivos da introdução

---
+++ cursoreact/01_intro/desafio_1/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Desafio n°1</h1>
+      <p>Opa, sou um parágrafo com um fundo colorido :D !</p>
+    </div>
+  );
+}
+
+export default App;
